41bf7de7c8ef44f53297cee1ec440153278e31d7
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-26 13:33:46 -0400

    In cuenta_regresiva.rb added input variables and commented out original loop

---
+++ cuenta_regresiva.rb
@@ -0,0 +1,10 @@
+
+puts 'Ingrese un número para comenzar la cuenta: '
+cuenta_regresiva = ARGV[0].to_i
+puts "Contando desde #{cuenta_regresiva}..."
+
+#original loop
+# until cuenta_regresiva < 0
+# puts cuenta_regresiva
+# cuenta_regresiva -= 1
+# end
